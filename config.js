4211fe50f554943225377ef629f42bad64fd7159
SNAPE-XD <tamseelriaz97@gmail.com>
2024-06-26 19:26:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -15,8 +15,8 @@ global.devs = '923096566451';
 global.website = 'https://king-md-session.onrender.com/' // Hello world
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/5541dfe4dc8c2f51e3f02.jpg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'KING-MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'Naveed Dogar' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? 'SNAPE-MD' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Tamseel Riaz' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
   author:  process.env.PACK_AUTHER.split(";")[0] === undefined ? 'Naveed-Dogar' : process.env.PACK_AUTHER.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS  || 'true' ,
